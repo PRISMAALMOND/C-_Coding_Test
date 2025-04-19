1270c1161822711d17bf708d8d36d8b3e1b94b99
dayayas2 <dayayas2@gmail.com>
2025-04-19 22:28:57 +0900

    feat: 001_문자열출력하기

---
+++ 001_문자열출력하기.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String s;
+
+        Console.Clear();
+        s = Console.ReadLine();
+        Console.WriteLine(s);
+    }
+}
+
+
+/*
+실행방법
+
+1. dotnet new console -n test
+2. cd test
+3. dotnet run
+4. 입력값 입력
+
+*/
